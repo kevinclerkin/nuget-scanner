180bc304c801c148a4879f31e79553d3a40e5774
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-06-24 23:12:24 +0100

    Add mock nupkg and nuspec data

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -8,3 +8,16 @@ from scanner import get_nuspec_metadata, check_vulnerabilities, scan_nuget_packa
 def create_mock_nupkg(file_path, nuspec_content):
     with zipfile.ZipFile(file_path, 'w') as zf:
         zf.writestr('package.nuspec', nuspec_content)
+
+
+def test_extract_nuspec_metadata(tmp_path):
+    nupkg_path = tmp_path / "test_package.nupkg"
+    nuspec_content = '''<?xml version="1.0"?>
+                        <package>
+                            <metadata>
+                                <id>TestPackage</id>
+                                <version>1.0.0</version>
+                            </metadata>
+                        </package>'''
+    create_mock_nupkg(nupkg_path, nuspec_content)
+    
